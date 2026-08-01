0c22bbdcae91b0aa638b55d0de2d3d12716a4b37
14lua <xhigoliluan022@gmail.com>
2026-08-01 02:01:12 +0200

    hyprland

---
+++ hypr/.config/hypr/modules/autostart.lua
@@ -0,0 +1,14 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+
+hl.on("hyprland.start", function()
+  hl.exec_cmd("waybar")
+  hl.exec_cmd("swaync")
+  hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("hypridle")
+end)
---
+++ hypr/.config/hypr/modules/monitors.lua
@@ -0,0 +1,10 @@
+-----------------
+---- MONITORS ---
+-----------------
+
+hl.monitor({
+  output   = "eDP-1",
+  mode     = "preferred",
+  position = "auto",
+  scale    = "1",
+})
